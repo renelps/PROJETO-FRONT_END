2443e6a14efeabf9f15c24f6a6a86a74de1f29c2
Renan Gabriel <renanlps616@gmail.com>
2024-05-05 19:43:43 -0300

    created filter and initial fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import Image from "next/image";
+"use client"
 import styles from "./page.module.css";
+import { FilterBar } from "@/components/filter-bar";
+import { ProductsList } from "@/components/products-list";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import styled from "styled-components";
 
+const FilterContainer = styled.main`
+  display: flex;
+  width: 100%;
+  align-items: start;
+`
 export default function Home() {
+  const client = new QueryClient();
   return (
-    <main className={styles.main}>
-    </main>
+    <QueryClientProvider client={client}>
+      <main className={styles.main}>
+        <FilterBar />
+        <ProductsList />
+      </main>
+    </QueryClientProvider>
+
   );
 }
